88ce8a128554329b40d89f3b6ff1f7bc5f34c46b
wgx <2997605851@qq.com>
2025-05-07 21:25:33 +0800

    fix:添加自动化流程

--- plugins/xlsx2json/1.1.0.js
+++ plugins/xlsx2json/1.1.0.js
@@ -1,5 +1,19 @@
 const xlsx = require('xlsx');
 
+function writingRules(inputArray, outputNodeTemplate) {
+  // 过滤出xlsx文件
+  const xlsxFiles = inputArray.filter(item => item.normExt === 'xlsx');
+
+  // 处理每个文件并生成输出节点
+  return xlsxFiles.map(file => ({
+    ...outputNodeTemplate,
+    normExt:'json',
+    content: JSON.stringify(readExcel(file)) // 读取Excel内容
+  }));
+
+  // return []
+}
+
 function readExcel(file) {
   try {
     // 1. 读取工作簿（启用公式计算）
@@ -27,6 +41,6 @@ function readExcel(file) {
 
 module.exports = {
   name: 'xlsx2json',
-  version: '1.0.0',
-  process: readExcel
+  version: '1.1.0',
+  process: writingRules
 };
---
+++ update-metadata.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+
+// 插件目录和元数据文件路径
+const pluginsDir = path.join(process.cwd(), 'plugins');
+const metadataPath = path.join(process.cwd(), 'metadata.json');
+
+// 获取单个插件的所有版本信息
+function getPluginVersions(pluginPath, pluginName) {
+  const files = fs.readdirSync(pluginPath).filter(f => f.endsWith('.js'));
+  const versions = {};
+  files.forEach(file => {
+    const version = file.replace('.js', '');
+    const relPath = `plugins/${pluginName}/${file}`;
+    versions[version] = {
+      gitee: `https://gitee.com/Geoffwo/rule-process-plugin/raw/master/${relPath}`,
+      github: `https://gitee.com/Geoffwo/rule-process-plugin/raw/master/${relPath}`
+    };
+  });
+  return versions;
+}
+
+// 收集所有插件信息
+function collectPlugins() {
+  const plugins = {};
+  fs.readdirSync(pluginsDir).forEach(pluginName => {
+    const pluginPath = path.join(pluginsDir, pluginName);
+    if (fs.statSync(pluginPath).isDirectory()) {
+      const versions = getPluginVersions(pluginPath, pluginName);
+      if (Object.keys(versions).length > 0) {
+        plugins[pluginName] = { versions };
+      }
+    }
+  });
+  return plugins;
+}
+
+// 写入 metadata.json
+function updateMetadata() {
+  const plugins = collectPlugins();
+  // fs.writeFileSync(metadataPath, JSON.stringify({ plugins }, null, 2), 'utf-8');
+  fs.writeFileSync(metadataPath, JSON.stringify(plugins, null, 2), 'utf-8');
+  console.log('metadata.json 已自动更新');
+}
+
+updateMetadata();
